16d2e1719c8bf4d00f94b7c5a2de6a1f7af7f87e
Daniel Won <dan.j.won@gmail.com>
2021-09-22 06:54:16 -0500

    add mini book project

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 // import React from 'react';
 import ReactDom from 'react-dom';
+import './index.css';
 
-function Greeting() {
-	return <h4>This is Dan, and this is my first component.</h4>;
+function BookList() {
+	return (
+		<section className='booklist'>
+			<Book />
+		</section>
+	);
 }
 
-ReactDom.render(<Greeting />, document.getElementById('root'));
+const author = 'Ann Whitford Paul';
+const Book = () => {
+	const title = 'If Animals Kissed Good Night';
+	return (
+		<article className='book'>
+			<img
+				src='https://images-na.ssl-images-amazon.com/images/I/81nzxODnaJL._AC_UL200_SR200,200_.jpg'
+				alt='cover of the kids book'
+			/>
+			<h1>{title}</h1>
+			<h4>{author.toUpperCase()}</h4>
+		</article>
+	);
+};
+
+ReactDom.render(<BookList />, document.getElementById('root'));
