7a4a29ff13374aeb6b0ed7b6ac572ea4ef7105ff
Daniel Won <dan.j.won@gmail.com>
2021-09-23 20:43:15 -0500

    add onClick and mouseOver events

--- src/index.js
+++ src/index.js
@@ -34,11 +34,29 @@ function BookList() {
 }
 
 const Book = ({ img, title, author }) => {
+	const clickHandler = () => {
+		alert('hello world');
+	};
+
 	return (
-		<article className='book'>
+		<article
+			className='book'
+			onMouseOver={() => {
+				console.log(title);
+			}}
+		>
 			<img src={img} alt='cover of the kids book' />
-			<h1>{title}</h1>
+			<h1
+				onClick={() => {
+					console.log(title);
+				}}
+			>
+				{title}
+			</h1>
 			<h4>{author}</h4>
+			<button type='button' onClick={clickHandler}>
+				reference example
+			</button>
 		</article>
 	);
 };
